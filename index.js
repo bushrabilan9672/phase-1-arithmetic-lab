6cb1667ea4978253f7a4e6228f73a095a61674cd
bushrabilan <bushrabilan9672@gmail.com>
2026-01-08 10:20:08 +0300

    d

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 
 // Write your code here
+// 1️⃣ multiply
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
+// 2️⃣ random
+let random = Math.floor(Math.random() * 10) + 1;
+
+// 3️⃣ mod
+let num3 = 14;
+let num4 = 5;
+let mod = num3 % num4
+
+// 4️⃣ max
+let max = Math.max(10, 15, 20);
